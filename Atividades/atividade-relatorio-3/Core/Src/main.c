f296d0344a56f1cf607cdbc3c78ec17fdacc9d5c
Coimbrax <lucascoimbralago@gmail.com>
2024-11-20 17:38:22 -0300

    Atualização do código - inclusão do button.h

--- Atividades/atividade-relatorio-3/Core/Src/main.c
+++ Atividades/atividade-relatorio-3/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "button.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
